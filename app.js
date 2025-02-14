264455fbb162d5a238c87e1a62f21a3016294231
sujal hinge <sujalhinge079@gmail.com>
2025-02-14 23:39:04 +0530

    dashboard

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const arrows = document.querySelectorAll(".arrow");
-const movieLists = document.querySelectorAll(".movie-list");
+const eventLists = document.querySelectorAll(".event-list");
 
 arrows.forEach((arrow, i) => {
-  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  const itemNumber = eventLists[i].querySelectorAll("img").length;
   let clickCounter = 0;
   arrow.addEventListener("click", () => {
     const ratio = Math.floor(window.innerWidth / 270);
     clickCounter++;
     if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      eventLists[i].style.transform = `translateX(${
+        eventLists[i].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
     } else {
-      movieLists[i].style.transform = "translateX(0)";
+      eventLists[i].style.transform = "translateX(0)";
       clickCounter = 0;
     }
   });
 
   console.log(Math.floor(window.innerWidth / 270));
 });
+
